0ea51ba6bc5b9fb6ddadd464a7e04f67e4b334bb
Nadine_06 <nadineescobar03@gmail.com>
2025-06-13 21:29:32 -0300

    Se implementa archivo data.js con datos

---
+++ data.js
@@ -0,0 +1,151 @@
+//Datos de frameworks
+export const frameworks = [
+  { nombre: 'JavaScript', icono: 'logos:javascript' },
+  { nombre: 'HTML5', icono: 'logos:html-5' },
+  { nombre: 'CSS3', icono: 'logos:css-3' },
+  { nombre: 'TailwindCSS', icono: 'logos:tailwindcss-icon' },
+  { nombre: 'SASS', icono: 'logos:sass' },
+  { nombre: 'React', icono: 'logos:react' },
+  { nombre: 'Node.js', icono: 'logos:nodejs-icon' },
+];
+
+//Datos de habilidades
+export const habilidades = [
+  'Trabajo en equipo',
+  'Pensamiento crítico',
+  'Gestión del tiempo',
+  'Resolución de problemas',
+  'Creatividad',
+  'Figma',
+  'Trello',
+  'Moqups',
+  'Github',
+  'VSCode',
+  'Canva'
+];
+
+// Datos de estudios
+export const estudios = [
+  {
+    titulo: 'Técnico Superior en Programación - Instituto TECLAB',
+    imagen: 'https://res.cloudinary.com/dgy7rqtqn/image/upload/w_300,f_auto,q_auto/v1748468833/t%C3%ADtulo-Prog_q4bwyo.png', 
+    imagenGrande: 'https://res.cloudinary.com/dgy7rqtqn/image/upload/v1748468833/t%C3%ADtulo-Prog_q4bwyo.png'
+  },
+  {
+    titulo: "Certificado de Prácticas Profesionalizante - Instituto TECLAB",
+    imagen: 'https://res.cloudinary.com/dgy7rqtqn/image/upload/w_300,f_auto,q_auto/v1748468832/pP_kwsyqh.jpg', 
+    imagenGrande: 'https://res.cloudinary.com/dgy7rqtqn/image/upload/v1748468832/pP_kwsyqh.jpg'
+  },
+  {
+    titulo: 'Desarrollo Web - Coderhouse',
+    imagen: 'https://res.cloudinary.com/dgy7rqtqn/image/upload/w_300,f_auto,q_auto/v1748468829/desarrolloWeb-Coderhouse_nup4ux.png',
+    imagenGrande: 'https://res.cloudinary.com/dgy7rqtqn/image/upload/v1748468829/desarrolloWeb-Coderhouse_nup4ux.png'
+  },
+  {
+    titulo: 'JavaScript - Coderhouse',
+    imagen: 'https://res.cloudinary.com/dgy7rqtqn/image/upload/w_300,f_auto,q_auto/v1749855218/9b6a5b42-6930-461f-b627-1508608fd8ac_eztnal.png',
+    imagenGrande: 'https://res.cloudinary.com/dgy7rqtqn/image/upload/v1749855218/9b6a5b42-6930-461f-b627-1508608fd8ac_eztnal.png'
+  },
+  {
+    titulo: 'Developer - No Country',
+    imagen: 'https://res.cloudinary.com/dgy7rqtqn/image/upload/w_300,f_auto,q_auto/v1748468831/nNo-country_rkswzs.jpg',
+    imagenGrande: 'https://res.cloudinary.com/dgy7rqtqn/image/upload/v1748468831/nNo-country_rkswzs.jpg'
+  },
+  {
+    titulo: 'Visualización de Datos - FreeCodeCamp',
+    imagen: 'https://res.cloudinary.com/dgy7rqtqn/image/upload/w_300,f_auto,q_auto/v1748468833/visu_Dat_jjckdf.png',
+    imagenGrande: 'https://res.cloudinary.com/dgy7rqtqn/image/upload/v1748468833/visu_Dat_jjckdf.png'
+  },
+  {
+    titulo: 'Diseño Web Responsive - FreeCodeCamp',
+    imagen: 'https://res.cloudinary.com/dgy7rqtqn/image/upload/w_300,f_auto,q_auto/v1748468834/web-responsi_vahjph.png',
+    imagenGrande: 'https://res.cloudinary.com/dgy7rqtqn/image/upload/v1748468834/web-responsi_vahjph.png'
+  },
+  {
+    titulo: 'Bibliotecas de Desarrollo Front-end - FreeCodeCamp',
+    imagen: 'https://res.cloudinary.com/dgy7rqtqn/image/upload/w_300,f_auto,q_auto/v1748468829/bibli-desa_front_ihxpmk.png',
+    imagenGrande: 'https://res.cloudinary.com/dgy7rqtqn/image/upload/v1748468829/bibli-desa_front_ihxpmk.png'
+  }
+];
+
+// Datos de proyectos
+export const proyectos = [
+  {
+    titulo: 'Web de recetas',
+    descripcion: 'Sitio de Recetas realizada en un proyecto de Desarrollo Web.',
+    link: 'https://github.com/NadineEscobar/Recetando-SASS',
+    imagen: 'https://res.cloudinary.com/dgy7rqtqn/image/upload/w_600,f_auto,q_auto/v1748468881/receteandoR-Sass_sfoik2.jpg',
+    tecnologias: [
+      { nombre: 'HTML', icono: 'logos:html-5' },
+      { nombre: 'SASS', icono: 'logos:sass' },
+      { nombre: 'JavaScript', icono: 'logos:javascript' }
+    ]
+  },
+  {
+    titulo: 'Mi Portafolio',
+    descripcion: 'Sitio de mi Portafolio personal',
+    link: 'https://mi-portafolio-virid-eight.vercel.app/',
+    imagen: 'https://res.cloudinary.com/dgy7rqtqn/image/upload/w_600,f_auto,q_auto/v1748474985/Captura_de_pantalla_28-5-2025_202928_127.0.0.1_mvcoag.jpg',
+    tecnologias: [
+      { nombre: 'HTML', icono: 'logos:html-5' },
+      { nombre: 'Tailwind', icono: 'logos:tailwindcss-icon' },
+      { nombre: 'JavaScript', icono: 'logos:javascript' }
+    ]
+  },
+  {
+    titulo: 'Ruleta personalizada',
+    descripcion: 'Ruleta interactiva desarrollada con JS y CSS con animación fluida de giro y detección aleatoria de resultados al deteners',
+    link: 'https://github.com/NadineEscobar/Ruleta-Customizable',
+    imagen: 'https://res.cloudinary.com/dgy7rqtqn/image/upload/w_600,f_auto,q_auto/v1748468882/ruleta_aytp1z.png',
+    tecnologias: [
+      { nombre: 'HTML', icono: 'logos:html-5' },
+      { nombre: 'CSS', icono: 'logos:css-3' },
+      { nombre: 'JavaScript', icono: 'logos:javascript' }
+    ]
+  },
+  {
+    titulo: 'Ta Te Ti',
+    descripcion: 'Un simple juego de Ta Te Ti, detección de ganador o empate y diseño interactivo y fácil de usar',
+    link: 'https://github.com/NadineEscobar/JuegoDelTaTeTi',
+    imagen: 'https://res.cloudinary.com/dgy7rqtqn/image/upload/w_600,f_auto,q_auto/v1748468883/taTeTi_bwfhqx.png',
+    tecnologias: [
+      { nombre: 'HTML', icono: 'logos:html-5' },
+      { nombre: 'CSS', icono: 'logos:css-3' },
+      { nombre: 'JavaScript', icono: 'logos:javascript' }
+    ]
+  },
+  {
+    titulo:'E-commerce',
+    descripcion: 'Proyecto de práctica de una página web sobre un almacen con sus distintos productos, ofertas, etc. Este proyecto se encuentra en proceso.',
+    link: 'https://github.com/NadineEscobar/e-commerce-almacen',
+    imagen: 'https://res.cloudinary.com/dgy7rqtqn/image/upload/w_600,f_auto,q_auto/v1748468879/e-commerce_mri8ai.png',
+    tecnologias: [
+      { nombre: 'HTML', icono: 'logos:html-5' },
+      { nombre: 'CSS', icono: 'logos:css-3' },
+      { nombre: 'JavaScript', icono: 'logos:javascript' }
+    ]
+  },
+  {
+    titulo: 'Simulador de Ahorros',
+    descripcion: 'Proyecto de simulador de ahorros, donde se puede calcular el monto total de ahorro al cabo de un tiempo determinado en pesos y dólares, con un diseño sencillo y fácil de usar.',
+    link: 'https://github.com/NadineEscobar/SimuladordeAhorro-ProyectoFinal-Escobar',
+    imagen: 'https://res.cloudinary.com/dgy7rqtqn/image/upload/w_600,f_auto,q_auto/v1748474378/Captura_de_pantalla_28-5-2025_201911_nadineescobar.github.io_iln6r1.jpg',
+    tecnologias: [
+      { nombre: 'HTML', icono: 'logos:html-5' },
+      { nombre: 'CSS', icono: 'logos:css-3' },
+      { nombre: 'JavaScript', icono: 'logos:javascript' }
+    ]
+  },
+  {
+    titulo: 'Lista de Tareas',
+    descripcion: 'Web de una Lista de Tareas pendientes sencillas',
+    link: 'https://github.com/NadineEscobar/Tareas-Pendientes-React-ts',
+    imagen: 'https://res.cloudinary.com/dgy7rqtqn/image/upload/w_600,f_auto,q_auto/v1748468880/lista-Tareas_exqdvj.jpg',
+    tecnologias: [
+      { nombre: 'HTML', icono: 'logos:html-5' },
+      { nombre: 'CSS', icono: 'logos:css-3' },
+      { nombre: 'JavaScript', icono: 'logos:javascript' },
+      { nombre: 'React', icono: 'logos:react' }
+    ]
+  }
+];
